b7ba613169eda0f783ec4e77d9bb91b47bf2e091
mmythily <mythily.mudunuru@gmail.com>
2020-01-31 11:50:32 -0500

    words counted after button press

--- src/App.js
+++ src/App.js
@@ -2,21 +2,33 @@ import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  const [textInput, setTextInput] = useState('')
 
+  //track the text in the textarea on each keystroke
+  const [textInput, setTextInput] = useState('')
+  
   function handleChange (event) {
-    setTextInput(event.target.value)
+    const {value} = event.target
+    setTextInput(value)
   }
 
-  console.log(textInput)
+  //calculate the number of words in the textInput
+  function countWords(textInput){
+    let wordsArr = textInput.trim().split(' ')
+    return wordsArr.length;
+  }
+
+  
+  
   return (
     <div className="App" >
       <h1>SPEED TYPER</h1>
       <textarea 
-        onChange={handleChange}/>
-      <button> Start </button>
+        onChange={handleChange}
+        value={textInput}/>
+      <button onClick={() => console.log(countWords(textInput))}> Start </button>
       <p>Time Remaining: 10 seconds</p>
       <p> Word Count: </p>
+      <p> Character Count: {textInput.length}</p>
     </div>
   );
 }

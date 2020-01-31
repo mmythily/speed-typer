578bc72467518027a78daeecba796c974de7f868
mmythily <mythily.mudunuru@gmail.com>
2020-01-31 11:15:36 -0500

    input text state on change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [textInput, setTextInput] = useState('')
+
+  function handleChange (event) {
+    setTextInput(event.target.value)
+  }
+
+  console.log(textInput)
   return (
     <div className="App" >
       <h1>SPEED TYPER</h1>
-      <textarea> </textarea>
+      <textarea 
+        onChange={handleChange}/>
       <button> Start </button>
       <p>Time Remaining: 10 seconds</p>
       <p> Word Count: </p>
